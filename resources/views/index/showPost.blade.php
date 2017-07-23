16af094ecfe0b2aec6cc362e4c0270d6de63cde5
Dominik <szymanowskidominik@gmail.com>
2017-07-23 14:14:06 +0200

    some code simplify

--- resources/views/index/showPost.blade.php
+++ resources/views/index/showPost.blade.php
@@ -45,7 +45,11 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       @foreach ($post->comments as $comment)
-        <h3 class="post-subtitle">{{$comment->name}} <h6>{{$comment->created_at}}<h6></h3>  <p>{{$comment->content}}</p>
+        <h3 class="post-subtitle">
+          {{$comment->name}}
+          <h6>{{$comment->created_at}}<h6>
+        </h3>
+        <p>{!!$comment->content!!}</p>
         <hr>
       @endforeach
     </div>
